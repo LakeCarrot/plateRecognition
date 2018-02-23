4507271d1903aa84ca4dee6ee1e48f9e3047c129
Bo Hu <b.hu@yale.edu>
2018-02-22 20:47:56 -0500

    gc in java

--- src/main/java/PlateServer.java
+++ src/main/java/PlateServer.java
@@ -104,6 +104,7 @@ public class PlateServer {
 			PlateRecognizer plate = new PlateRecognizer();
 			plate.recognize(filename);
 			long end = System.currentTimeMillis();
+			plate = null;
 			PlateRecognitionReply reply = PlateRecognitionReply.newBuilder()
 				.setMessage("You shall not pass!")
 				.build();
--- src/main/java/platerecognizer/PlateRecognizer.java
+++ src/main/java/platerecognizer/PlateRecognizer.java
@@ -42,5 +42,6 @@ public class PlateRecognizer {
 			}
 			*/
 			//alpr.unload();
+			alpr = null;
 		}
 }
